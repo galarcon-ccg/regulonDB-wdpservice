66b6c71981f0e148e5d9fdc547e824783042e6b1
galarcon-ccg <galarcon@ccg.unam.mx>
2022-01-27 08:46:23 -0600

    fix import and env problems

--- api/__init__.py
+++ api/__init__.py
@@ -7,19 +7,43 @@ from api.processes_HT.authorData import formatData_to_json_author_table
 
 app = Flask(__name__)
 CORS(app)
-gql_service = HTTPEndpoint(os.environ.get("GQL_SEVICE"))
+gql_url = os.environ.get("GQL_SERVICE")
+gql_service = HTTPEndpoint(gql_url)
+
+
+@app.route('/')
+def index():
+    return "welcome :)"
+
+
+@app.route('/process/ht-dataset/<id_dataset>/<data_type>/<file_format>')
+def process_ht(id_dataset, data_type, file_format):
+    ht_process = HTprocess(id_dataset, gql_service)
+    response = 'error'
+    if data_type == 'authorData':
+        ht_process.authorData(file_format)
+        return ht_process.get_response()
+    return response
+
+
+'''
+
+
+
+
 
 @app.route('/process/ht-dataset/<datasetId>/<type>/<format>')
-def process_ht(datasetId,type,format):
-    ht_process = HTprocess(datasetId,gql_service)
+def process_ht(datasetId, type, format):
+    ht_process = HTprocess(datasetId, gql_service)
     response = 'error'
     if type == 'authorData':
         ht_process.authorData(format)
     return response
 
+
 @app.route('/<datasetId>')
 def author_data(datasetId):
-    variables = {"datasetId": ""+datasetId}
+    variables = {"datasetId": "" + datasetId}
     query = """query AuthorsDataOfDataset($datasetId: String!)
     {
         getAuthorsDataOfDataset(datasetId: $datasetId){
@@ -39,3 +63,5 @@ def author_data(datasetId):
         mimetype='application/json'
     )
     return response
+
+'''
--- api/processes_HT/ht_process.py
+++ api/processes_HT/ht_process.py
@@ -1,22 +1,31 @@
-from typing_extensions import Self
-from querys import Querys
-
+from flask import send_file
+from .querys import Querys
 
 
 class HTprocess:
-    
     querys = Querys()
-    
-    def __init__(self, datasetId, gql_service):
-        self.datasetId = datasetId
-        self.variables = {"datasetId": ""+datasetId}
-    
-    def authorData(self, format):
+
+    def __init__(self, id_dataset, gql_service):
+        self.id_dataset = id_dataset
+        self.gql_service = gql_service
+        self.ht_response = ""
+
+    def authorData(self, file_format):
         query = self.querys.AuthorsDataOfDataset
-        if format == 'cvs' or format == 'CVS':
-            return ',,'
-        elif format == 'jsontable' or format == 'JSONtable' or format == 'jsonTable':
-            #JSONtable process
-            return {}
+        variables = {"datasetId": "" + self.id_dataset}
+        data = self.gql_service(query, variables)
+        try:
+            data = data['data']['getAuthorsDataOfDataset'][0]['authorsData']
+        except Exception as e:
+            print(e)
+            data = {}
+        if file_format == 'cvs' or file_format == 'CVS':
+            self.ht_response = data
+        elif file_format == 'jsontable' or file_format == 'JSONtable' or file_format == 'jsonTable':
+            # JSONtable process
+            self.ht_response = {}
         else:
-            return 'invalid format: '+format
+            self.ht_response = 'invalid format: ' + file_format
+
+    def get_response(self):
+        return self.ht_response
